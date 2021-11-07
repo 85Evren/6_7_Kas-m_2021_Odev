5da639df979e2917a99d88f3a674fefee896cdc4
Evren Kasap <evrkll9@gmail.com>
2021-11-07 13:11:12 +0300

    Add project files.

---
+++ 6_7_Kasım_2021_Odev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_7_Kasım_2021_Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
